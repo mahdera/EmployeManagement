1d7a08b598749bcc2bee7b5f97946a5ce8aa5709
Mahder Neway <mahderalem@gmail.com>
2015-02-18 09:49:49 -0500

    project clean passed

--- src/main/java/com/mahder/employeemanagement/controller/DataController.java
+++ src/main/java/com/mahder/employeemanagement/controller/DataController.java
@@ -5,8 +5,14 @@ package com.mahder.employeemanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.mahder.employeemanagement.domain.Employee;
+import com.mahder.employeemanagement.service.DataService;
+
 import java.util.List;
 
 /**
@@ -26,32 +32,32 @@ public class DataController {
 	  
 	 @RequestMapping("register")  
 	 public ModelAndView registerUser(@ModelAttribute Employee employee) {  
-	  dataService.insertRow(employee);  
+	  dataService.saveEmployee(employee);  
 	  return new ModelAndView("redirect:list");  
 	 }  
 	  
 	 @RequestMapping("list")  
 	 public ModelAndView getList() {  
-	  List employeeList = dataService.getList();  
+	  List<Employee> employeeList = dataService.listEmployee();  
 	  return new ModelAndView("list", "employeeList", employeeList);  
 	 }  
 	  
 	 @RequestMapping("delete")  
 	 public ModelAndView deleteUser(@RequestParam int id) {  
-	  dataService.deleteRow(id);  
+	  dataService.deleteEmployee(id);  
 	  return new ModelAndView("redirect:list");  
 	 }  
 	  
 	 @RequestMapping("edit")  
 	 public ModelAndView editUser(@RequestParam int id,  
 	   @ModelAttribute Employee employee) {  
-	  Employee employeeObject = dataService.getRowById(id);  
+	  Employee employeeObject = dataService.getEmployee(id);  
 	  return new ModelAndView("edit", "employeeObject", employeeObject);  
 	 }  
 	  
 	 @RequestMapping("update")  
 	 public ModelAndView updateUser(@ModelAttribute Employee employee) {  
-	  dataService.updateRow(employee);  
+	  dataService.updateEmployee(employee);  
 	  return new ModelAndView("redirect:list");  
 	 }  
 	 
